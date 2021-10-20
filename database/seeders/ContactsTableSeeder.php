93ff5b3a18134ff6a40c3613a62269e183c369f5
e13 <isv@cwtm.ru>
2021-10-20 11:18:08 +0300

    initial commit

---
+++ database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Contact::class, 10)->create();
+    }
+}
